9b781a0c2e674a112ba592f5eae162b140737569
SlavBro <iamslavon@gmail.com>
2017-03-18 17:24:44 +0200

    Creating repo

---
+++ SH.Sensor.ino
@@ -0,0 +1,125 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include "DHT.h"
+
+#define DHTPIN 14
+#define DHTTYPE DHT22
+
+DHT dht(DHTPIN, DHTTYPE);
+WiFiServer server(80);
+
+const char* ssid = "Ministry of Pies";
+const char* password = "ilovethisplace";
+const String host = "37.57.107.136";
+const String postUrl = "http://37.57.107.136/data/set";
+IPAddress ip(192, 168, 1, 50);
+IPAddress gateway(192, 168, 1, 1);
+IPAddress subnet(255, 255, 255, 0);
+const int houseId = 1;
+const int roomId = 1;
+const int temperatureSensorId = 1;
+const int huminitySensorId = 2;
+
+struct Dht22 {
+	int Temperature;
+	int Humidity;
+};
+
+void setup() {
+	Serial.begin(115200);
+	delay(100);
+	dht.begin();
+	ConnectToWifi();
+	server.begin();
+}
+
+void loop() {
+	WiFiClient client = server.available();
+	if (!client) {
+		return;
+	}
+
+	while (!client.available()) {
+		delay(1);
+	}
+
+	String request = client.readStringUntil('\r');
+	client.flush();
+
+	if (request.indexOf("/getdata") != -1) {
+		Dht22 data;
+
+		do {
+			data = ReadDht();
+		} while (data.Humidity > 100 || data.Temperature > 100);
+		
+		PostDhtData(data);
+	}
+	else {
+		Serial.println("invalid request");
+		client.stop();
+		return;
+	}
+
+	client.flush();
+}
+
+void ConnectToWifi() {
+	Serial.print("Connecting to ");
+	Serial.println(ssid);
+	WiFi.begin(ssid, password);
+	WiFi.config(ip, gateway, subnet);
+
+	while (WiFi.status() != WL_CONNECTED) {
+		delay(500);
+		Serial.print(".");
+	}
+
+	Serial.println("");
+	Serial.println("WiFi connected");
+	Serial.println("IP address: ");
+	Serial.println(WiFi.localIP());
+}
+
+void PostDhtData(Dht22 data) {
+	String json = ComposeJsonForDht(data);
+
+	HTTPClient http;
+	http.begin(postUrl);
+	http.addHeader("Host", host);
+	http.addHeader("Content-Type", "application/json");
+	http.POST(json);
+	http.end();
+}
+
+String CreateJson(int houseId, int roomId, int sensorId, int value) {
+	String json = "{HouseId: ";
+	json += houseId;
+	json += ", RoomId: ";
+	json += roomId;
+	json += ", SensorId: ";
+	json += sensorId;
+	json += ", Value: ";
+	json += value;
+	json += "}";
+
+	return json;
+}
+
+String ComposeJsonForDht(Dht22 data) {
+	String json = "[";
+	json += CreateJson(houseId, roomId, huminitySensorId, data.Humidity);
+	json += ", ";
+	json += CreateJson(houseId, roomId, temperatureSensorId, data.Temperature);
+	json += "]";
+
+	return json;
+}
+
+Dht22 ReadDht() {
+	Dht22 dht22;
+	dht22.Humidity = dht.readHumidity();
+	dht22.Temperature = dht.readTemperature();
+
+	return dht22;
+}
